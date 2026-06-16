16d9ff742a63571a02eef773d3e5581688fdffb3
Anuja <anujapawar791@gmail.com>
2026-06-16 18:57:27 +0530

    ecr file created

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,15 @@ output "cluster_endpoint" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "frontend_ecr_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+output "backend_ecr_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "mongodb_ecr_url" {
+  value = aws_ecr_repository.mongodb.repository_url
+}
