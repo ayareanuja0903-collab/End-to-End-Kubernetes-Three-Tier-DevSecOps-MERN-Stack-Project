0c229159a41004997ed75efacbd994f6376f15f3
Anuja <anujapawar791@gmail.com>
2026-06-17 15:55:53 +0530

    DevSecOps project setup

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,7 +5,8 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  enable_irsa = true
+  enable_irsa                              = true
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -36,7 +37,7 @@ module "eks" {
       min_size     = 2
       max_size     = 4
       desired_size = 3
-      
+
 
       instance_types = ["t3.medium"]
 
