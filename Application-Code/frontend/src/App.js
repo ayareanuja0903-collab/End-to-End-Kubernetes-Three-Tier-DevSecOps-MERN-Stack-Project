8af39eca2d582ff9a3da07ea8c7bde5c7d183ff4
Anuja <anujapawar791@gmail.com>
2026-06-19 13:56:03 +0530

    app.js file edited

--- Application-Code/frontend/src/App.js
+++ Application-Code/frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>My To-Do List 29 June 2026</h1>
+                    <h1>My To-Do List 30 june 2026</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
