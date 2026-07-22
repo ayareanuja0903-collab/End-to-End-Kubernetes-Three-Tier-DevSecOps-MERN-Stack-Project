19ba7700ff37184d76d474e4694ed186f7767ab4
Ubuntu <ubuntu@ip-10-0-1-92.ap-south-1.compute.internal>
2026-07-22 13:43:42 +0530

    Fix MongoDB authentication with authSource

--- Application-Code/backend/db.js
+++ Application-Code/backend/db.js
@@ -5,18 +5,19 @@ module.exports = async () => {
         const connectionParams = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            authSource: "admin",
         };
-        const useDBAuth = process.env.USE_DB_AUTH || false;
-        if(useDBAuth){
+
+        if (process.env.USE_DB_AUTH === "true") {
             connectionParams.user = process.env.MONGO_USERNAME;
             connectionParams.pass = process.env.MONGO_PASSWORD;
         }
-        await mongoose.connect(
-           process.env.MONGO_CONN_STR,
-           connectionParams
-        );
+
+        await mongoose.connect(process.env.MONGO_CONN_STR, connectionParams);
+
         console.log("Connected to database.");
     } catch (error) {
-        console.log("Could not connect to database.", error);
+        console.error("Could not connect to database.", error);
+        process.exit(1);
     }
 };
