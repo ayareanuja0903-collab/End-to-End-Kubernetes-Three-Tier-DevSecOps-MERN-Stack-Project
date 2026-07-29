722bc3be7abb5bd829e75e63e2589c133a3c85c9
Anuja <anujapawar791@gmail.com>
2026-07-29 15:13:48 +0530

    Refactor services to ClusterIP for single ALB architecture

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,27 +15,15 @@ variable "cluster_version" {
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  type = string
 }
 
 variable "public_subnets" {
-
   type = list(string)
-
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
 }
 
 variable "private_subnets" {
-
   type = list(string)
-
-  default = [
-    "10.0.11.0/24",
-    "10.0.12.0/24"
-  ]
 }
 
 variable "instance_type" {
