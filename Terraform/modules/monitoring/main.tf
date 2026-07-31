3f0d78e005bb28ba6e52ddb647edb83d5f4de8fb
Anuja <anujapawar791@gmail.com>
2026-07-31 14:29:21 +0530

    update the terraform monitoring main.tf code

--- Terraform/modules/monitoring/main.tf
+++ Terraform/modules/monitoring/main.tf
@@ -7,56 +7,38 @@ resource "kubernetes_namespace" "monitoring" {
 }
 
 resource "helm_release" "kube_prometheus_stack" {
-
-  name       = "kube-prometheus-stack"
-  namespace  = "monitoring"
-
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-
+  name             = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
   create_namespace = true
 
   values = [
+    yamlencode({
+      grafana = {
+        adminUser     = "admin"
+        adminPassword = "Admin@123"
 
-yamlencode({
-
-  grafana = {
-
-    adminUser     = "admin"
-    adminPassword = "Admin@123"
-
-    grafana.ini = {
-      server = {
-        root_url            = "%(protocol)s://%(domain)s/grafana/"
-        serve_from_sub_path = true
+        "grafana.ini" = {
+          server = {
+            root_url            = "%(protocol)s://%(domain)s/grafana/"
+            serve_from_sub_path = true
+          }
+        }
       }
-    }
 
-  }
+      prometheus = {
+        prometheusSpec = {
+          externalUrl = "http://%(domain)s/prometheus"
+          routePrefix = "/prometheus"
+        }
+      }
 
-  prometheus = {
-
-    prometheusSpec = {
-
-      externalUrl = "http://%(domain)s/prometheus"
-
-      routePrefix = "/prometheus"
-
-    }
-
-  }
-
-  alertmanager = {
-
-    alertmanagerSpec = {
-
-      externalUrl = "http://%(domain)s/alertmanager"
-
-    }
-
-  }
-
-})
-
+      alertmanager = {
+        alertmanagerSpec = {
+          externalUrl = "http://%(domain)s/alertmanager"
+        }
+      }
+    })
   ]
 }
