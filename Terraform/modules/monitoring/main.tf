ad8fb8e15fdff290b414f1985de6d1ff23150ac0
Anuja <anujapawar791@gmail.com>
2026-07-31 17:03:11 +0530

    Fix monitoring route code update

--- Terraform/modules/monitoring/main.tf
+++ Terraform/modules/monitoring/main.tf
@@ -7,102 +7,53 @@ resource "kubernetes_namespace" "monitoring" {
 }
 
 resource "helm_release" "kube_prometheus_stack" {
-
   name             = "kube-prometheus-stack"
   namespace        = "monitoring"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
   create_namespace = true
 
-  wait    = true
-  timeout = 900
-
   values = [
     yamlencode({
-
       grafana = {
-
-        enabled = true
-
         adminUser     = "admin"
         adminPassword = "Admin@123"
 
-        service = {
-          type = "ClusterIP"
-        }
-
-        persistence = {
-          enabled          = true
-          storageClassName = "gp2"
-          size             = "10Gi"
-        }
-
         "grafana.ini" = {
-
           server = {
-
             root_url            = "%(protocol)s://%(domain)s/grafana/"
             serve_from_sub_path = true
-
           }
-
         }
-
       }
-
 
       prometheus = {
-
-        enabled = true
-
-        service = {
-          type = "ClusterIP"
-        }
-
         prometheusSpec = {
-
           routePrefix = "/prometheus"
-
-          externalUrl = "/prometheus"
-
+           externalUrl = "/prometheus"
         }
-
       }
-
 
       alertmanager = {
-
-        enabled = true
-
-        service = {
-          type = "ClusterIP"
-        }
-
         alertmanagerSpec = {
-
           routePrefix = "/alertmanager"
-
-          externalUrl = "/alertmanager"
-
+          externalUrl = ""
         }
-
       }
-
-
-      nodeExporter = {
-
-        enabled = true
-
-      }
-
-
+      
       kubeStateMetrics = {
-
         enabled = true
-
       }
-
+      
+      nodeExporter = {
+        enabled = true
+      }
+      
+      "prometheus-node-exporter" = {
+        enabled = true
+      }
+      
+      cleanupCustomResource = true
     })
   ]
-
 }
