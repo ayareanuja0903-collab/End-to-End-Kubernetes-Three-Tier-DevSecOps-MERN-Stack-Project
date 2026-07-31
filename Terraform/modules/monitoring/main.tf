5c1244ce8a8171d4a835f9754f34561fae9cdc48
Anuja <anujapawar791@gmail.com>
2026-07-31 14:22:08 +0530

    update the terraform monitoring main.tf code

--- Terraform/modules/monitoring/main.tf
+++ Terraform/modules/monitoring/main.tf
@@ -7,39 +7,56 @@ resource "kubernetes_namespace" "monitoring" {
 }
 
 resource "helm_release" "kube_prometheus_stack" {
+
   name       = "kube-prometheus-stack"
   namespace  = "monitoring"
+
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
 
   create_namespace = true
 
   values = [
-    yamlencode({
-      grafana = {
-        enabled       = true
-        adminUser     = "admin"
-        adminPassword = "Admin@123"
 
-        service = {
-          type = "ClusterIP"
-        }
+yamlencode({
 
-        persistence = {
-          enabled          = true
-          storageClassName = "gp2"
-          size             = "10Gi"
-        }
+  grafana = {
 
-        grafana = {
-          ini = {
-            server = {
-              root_url            = "%(protocol)s://%(domain)s/grafana/"
-              serve_from_sub_path = true
-            }
-          }
-        }
+    adminUser     = "admin"
+    adminPassword = "Admin@123"
+
+    grafana.ini = {
+      server = {
+        root_url            = "%(protocol)s://%(domain)s/grafana/"
+        serve_from_sub_path = true
       }
-    })
+    }
+
+  }
+
+  prometheus = {
+
+    prometheusSpec = {
+
+      externalUrl = "http://%(domain)s/prometheus"
+
+      routePrefix = "/prometheus"
+
+    }
+
+  }
+
+  alertmanager = {
+
+    alertmanagerSpec = {
+
+      externalUrl = "http://%(domain)s/alertmanager"
+
+    }
+
+  }
+
+})
+
   ]
 }
