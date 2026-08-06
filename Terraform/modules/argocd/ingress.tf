3adc1bd7dd2ceb885f5f447cb97b43e2e8e0629b
Anuja <anujapawar791@gmail.com>
2026-08-06 16:18:25 +0530

    Manage ArgoCD ingress using Terraform

---
+++ Terraform/modules/argocd/ingress.tf
@@ -0,0 +1,46 @@
+resource "kubernetes_ingress_v1" "argocd" {
+  metadata {
+    name      = "argocd-ingress"
+    namespace = "argocd"
+
+    annotations = {
+      "alb.ingress.kubernetes.io/group.name"      = "three-tier-platform"
+      "alb.ingress.kubernetes.io/group.order"     = "20"
+      "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"     = "ip"
+      "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTP\":80}]"
+      "alb.ingress.kubernetes.io/backend-protocol" = "HTTP"
+
+       
+      "alb.ingress.kubernetes.io/healthcheck-path" = "/argocd/api/version"
+      "alb.ingress.kubernetes.io/healthcheck-port" = "80"
+    }
+  }
+
+  spec {
+    ingress_class_name = "alb"
+
+    rule {
+      http {
+        path {
+          path      = "/argocd"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "argocd-server"
+
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
